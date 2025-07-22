5ac378bf276cccc966abb7282bbdd0c39ca598f9
Sodipto Saha <badhansaha444@gmail.com>
2025-07-22 21:25:23 +0600

    get user by id

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -17,8 +17,8 @@ def get_me():
     return {"username": "sodipto", "email": "sodipto.saha@asthait.com"}
 
 
-@router.get("/{user_id}", summary="Get user by ID", response_model=dict)
+@router.get("/{id}", summary="Get user by ID", response_model=dict)
 @inject
-async def get_user_by_id(user_id: int, user_service: UserService = Depends(Provide[Container.user_service])):
-    username = await user_service.get_user_name(user_id)
-    return {"username": username, "user_id": user_id}
+async def get_user_by_id(id: int, user_service: UserService = Depends(Provide[Container.user_service])):
+    username = await user_service.get_user_name(id)
+    return {"username": username, "user_id": id}

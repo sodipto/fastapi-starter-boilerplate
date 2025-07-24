8f6604317ed555d38688e87dda487097a16230b8
Sodipto Saha <badhansaha444@gmail.com>
2025-07-24 16:23:33 +0600

    added open api configuration

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from app.core.container import Container
 from app.core.config import settings
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 from app.api.v1.routes import routers as v1_routers
 
@@ -11,6 +12,28 @@ app = FastAPI(
     docs_url="/swagger",
     redoc_url="/redoc",
 )
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title=app.title,
+        version=app.version,
+        description=app.description,
+        routes=app.routes,
+    )
+    openapi_schema["components"]["securitySchemes"] = {
+        "Bearer": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+            "description": "Input your Bearer token to access all endpoints"
+        }
+    }
+    openapi_schema["security"] = [{"Bearer": []}]
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
 app.container = Container()
 
 app.include_router(v1_routers, prefix="/api/v1")

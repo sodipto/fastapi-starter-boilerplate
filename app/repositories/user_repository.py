91d5ab3dbecf1434d7a0e3177d4e55e0c97047c3
Sodipto Saha <badhansaha444@gmail.com>
2025-07-28 19:50:42 +0600

    removed print function

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,7 +1,6 @@
 from sqlalchemy import select
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import Optional, Dict
 import uuid
 
 from app.models.user import User
@@ -13,12 +12,10 @@ class UserRepository:
     async def get_user_by_email(self, email: str) -> User | None:
         result = await self.db.execute(select(User).where(User.Email == email))
         user = result.scalars().first()
-        print("User fetched:", user)
         return user
     
     async def get_user_by_id(self, id: uuid.UUID) -> User | None:
         print(f"Fetching user name for id: {id}")
         result = await self.db.execute(select(User).where(User.Id == str(id)))
         user = result.scalars().first()
-        print("User fetched:", user)
         return user

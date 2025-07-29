fe4129cf086006465ce25651d798cf886d784cd7
Sodipto Saha <badhansaha444@gmail.com>
2025-07-29 11:43:49 +0600

    login with email and password

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,7 +1,7 @@
 import uuid
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from dependency_injector.wiring import inject, Provide
+from pydantic import BaseModel,EmailStr, Field
 
 from app.core.container import Container
 from app.services.auth_service import AuthService
@@ -12,20 +12,22 @@ router = APIRouter(
     tags=["Authentication"]
 )
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/auth/login")
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=6, max_length=8)
 
 @router.post("/login")
 @inject
 async def login(
-    form_data: OAuth2PasswordRequestForm = Depends(),
+    payload: LoginRequest,
     auth_service: AuthService = Depends(Provide[Container.auth_service])
 ):
-    user = await auth_service.authenticate_user(form_data.username, form_data.password)
+    user = await auth_service.authenticate_user(payload.email, payload.password)
     
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Incorrect username or password!",
             headers={"WWW-Authenticate": "Bearer"},
         )
 

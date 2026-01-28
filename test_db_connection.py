8903920e4670a56c39d86cc5ba74d8f6c23d5b94
Sodipto Saha <badhansaha444@gmail.com>
2026-01-28 20:43:00 +0600

    remove old test_db_connection file

--- test_db_connection.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import asyncpg
-
-async def test_connection():
-    conn = await asyncpg.connect("postgresql://postgres:123456@localhost:5432/fastapi_boilerplate_dev")
-    print("✅ Connected to DB successfully!")
-    await conn.close()
-
-asyncio.run(test_connection())
